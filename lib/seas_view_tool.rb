131e9fecd27c3c54b9f7bd1a386a07b937ef0a79
Eddie Mares <eddiem.@eddies-mbp.attlocal.net>
2018-05-01 20:51:20 -0700

    Seas tool initial commit

---
+++ lib/seas_view_tool.rb
@@ -0,0 +1,6 @@
+require "seas_view_tool/version"
+require "seas_view_tool/renderer"
+
+module SeasViewTool
+  # Your code goes here...
+end
---
+++ lib/seas_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SeasViewTool
+  VERSION = "0.1.0"
+end
